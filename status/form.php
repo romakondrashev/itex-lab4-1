1dbfb9315f68bd03b042fd81296dda9ea9f33b26
Kondrashev Roman <43863997+romakondrashev@users.noreply.github.com>
2019-12-18 19:37:59 +0200

    Create form.php

---
+++ status/form.php
@@ -0,0 +1,6 @@
+<p style="color: red">Введите имя пользователя!</p>
+<form method="POST">
+	<label for="username">Имя пользователя</label>
+	<input type="text" name="username" id="username">
+	<input type="submit">
+</form>
