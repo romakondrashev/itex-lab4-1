ea9fb36003249e2578e26d75ec7954dbbdf2fd3e
Kondrashev Roman <43863997+romakondrashev@users.noreply.github.com>
2019-12-18 19:38:11 +0200

    Add files via upload

---
+++ status/info.php
@@ -0,0 +1,5 @@
+<p style="color: green">Добро пожаловать, <?= $_SESSION['username']; ?>!</p>
+<a href="reset.php">Выйти</a><br>
+<a href="index.php">Главная</a>
+<a href="contact.php">Контакты</a>
+<a href="archive.php">Новости</a>
